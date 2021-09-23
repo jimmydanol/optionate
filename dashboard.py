6d4f855bb73d6588171d889110112f8eee85c88d
Jimmy Stein <jimmydanol@gmail.com>
2021-09-23 09:05:46 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ This is some _markdown_.
 df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
 st.dataframe(df)
 
-st.sidebar.write(df)
+#st.sidebar.write(df)
 
 hide_st_style = """
             <style>
