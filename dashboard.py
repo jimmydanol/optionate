b5e1958153035d6379b80c26b7f18fc3d9fe04f4
Jimmy Stein <jimmydanol@gmail.com>
2021-09-24 12:43:37 -0400

    9

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,14 @@
+import config 
 import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
 import tweepy
+
+auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
 
 # st.title("This is the Nonsense")
 
@@ -23,6 +29,23 @@ df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(
 st.sidebar.title("Options")
 
 option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'))
+
+st.header(option)
+
+if option == 'twitter':
+    st.subheader('twitter dashboard logic')
+    user = st.sidebar.text_input("User", value='traderstewie')
+
+    tweets = api.user_timeline(user)
+    for tweet in tweets:
+        if '$' in tweet.text:
+            words = tweet.text.split(' ')
+            for word in words:
+                if word.startswith('$') and word[1:].isalpha():
+                    symbol = word[1:]
+                    st.write(symbol)
+                    st.write(tweet.text)
+                    st.image(f"https://finviz.com/chart.ashx?t={symbol}")
 
 if option == 'stocktwits':
     symbol = st.sidebar.text_input("Symbol", value='AAPL', max_chars=5)
