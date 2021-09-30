b6dfccece97ab66dddf57d409347b7277a8ae64d
Jimmy Stein <jimmydanol@gmail.com>
2021-09-30 16:59:16 -0400

    13

--- dashboard.py
+++ dashboard.py
@@ -11,8 +11,8 @@ auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_S
 
 api = tweepy.API(auth)
 
-connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
-cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+# connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+# cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # st.title("This is the Nonsense")
 
@@ -75,40 +75,40 @@ if option == 'stocktwits':
         st.write(message['body'])
 
 
-if option == 'wallstreetbets':
-    num_days = st.sidebar.slider('Number of days', 1, 30, 3)
+# if option == 'wallstreetbets':
+#     num_days = st.sidebar.slider('Number of days', 1, 30, 3)
 
-    cursor.execute("""
-        SELECT COUNT(*) AS num_mentions, symbol
-        FROM mention JOIN stock ON stock.id = mention.stock_id
-        WHERE date(dt) > current_date - interval '%s day'
-        GROUP BY stock_id, symbol   
-        HAVING COUNT(symbol) > 10
-        ORDER BY num_mentions DESC
-    """, (num_days,))
+#     cursor.execute("""
+#         SELECT COUNT(*) AS num_mentions, symbol
+#         FROM mention JOIN stock ON stock.id = mention.stock_id
+#         WHERE date(dt) > current_date - interval '%s day'
+#         GROUP BY stock_id, symbol   
+#         HAVING COUNT(symbol) > 10
+#         ORDER BY num_mentions DESC
+#     """, (num_days,))
 
-    counts = cursor.fetchall()
-    for count in counts:
-        st.write(count)
+#     counts = cursor.fetchall()
+#     for count in counts:
+#         st.write(count)
     
-    cursor.execute("""
-        SELECT symbol, message, url, dt, source
-        FROM mention JOIN stock ON stock.id = mention.stock_id
-        ORDER BY dt DESC
-        LIMIT 100
-    """)
+#     cursor.execute("""
+#         SELECT symbol, message, url, dt, source
+#         FROM mention JOIN stock ON stock.id = mention.stock_id
+#         ORDER BY dt DESC
+#         LIMIT 100
+#     """)
 
-    mentions = cursor.fetchall()
-    for mention in mentions:
-        st.text(mention['dt'])
-        st.text(mention['symbol'])
-        st.text(mention['message'])
-        st.text(mention['url'])
-        st.text(mention['source'])
+#     mentions = cursor.fetchall()
+#     for mention in mentions:
+#         st.text(mention['dt'])
+#         st.text(mention['symbol'])
+#         st.text(mention['message'])
+#         st.text(mention['url'])
+#         st.text(mention['source'])
 
-    rows = cursor.fetchall()
+#     rows = cursor.fetchall()
 
-    st.write(rows)
+#     st.write(rows)
 
 if option == 'mortgage':
     # st.subheader('coming soon to a screen near deez nutz')
